561b84ce46ae85ccfe9938003053b017aac327a5
wbagleyAsu <wbagley01@gmail.com>
2024-09-14 17:38:20 -0700

    Function Components

---
+++ src/components/Greet.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Function Declaration
+function Greet() {
+    return <h1>Greetings Humans</h1>
+}
+
+// Arrow Syntax
+//const Greet = () => <h1>Greetings Humans</h1>
+export default Greet
