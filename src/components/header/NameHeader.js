6308dbacc34a2bcb0bcc1fb1f0cb4d4e4a81e7a3
wbagleyAsu <wbagley01@gmail.com>
2024-09-15 17:40:52 -0700

    Update NameHeader.js

--- src/components/header/NameHeader.js
+++ src/components/header/NameHeader.js
@@ -7,7 +7,7 @@ function NameHeader() {
         <div className='NameHeader'>
             <div className='Name'>William Bagley</div>
             <div className='Container'>
-                <div className='Description'>Description</div>
+                <div className='Competitive Alpha Geek'>Description</div>
                 <img src= '/images/william.png' className='RealImage' alt=''/>
             </div>
             <div className='Container'>
