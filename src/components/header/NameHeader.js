4db048258c9f01d7ff586e20b5ba714dc7350a38
wbagleyAsu <wbagley01@gmail.com>
2024-09-17 13:50:08 -0700

    Basic click events

--- src/components/header/NameHeader.js
+++ src/components/header/NameHeader.js
@@ -10,8 +10,13 @@ function NameHeader() {
                 <div className='Description'>Competitive Alpha Geek</div>
                 <img src= '/images/william.png' className='RealImage' alt=''/>
             </div>
-            <div className='Container'>
-                
+            <div className='ImageIcons'>
+                <a href='https://www.linkedin.com/in/william-bagley-179326230/'>
+                    <img src= '/images/linkedin.png'/>
+                </a>
+                <a href='https://github.com/wbagley01'>
+                    <img src= '/images/github.png'/>
+                </a>
             </div>
         </div>
     )
