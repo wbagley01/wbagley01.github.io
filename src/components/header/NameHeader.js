05859d8c2bca2e58f5e0bc5d702ed3963b3d2b47
wbagleyAsu <wbagley01@gmail.com>
2024-09-26 21:35:52 -0700

    Update NameHeader.js
    Basic name header complete

--- src/components/header/NameHeader.js
+++ src/components/header/NameHeader.js
@@ -3,11 +3,24 @@ import App from '../../App'
 import './NameHeader.css'
 
 function NameHeader() {
+    const email = "wbagley01@gmail.com";
+    const descriptionText = "Here's a story all about how my life got"
+
+    const handleImageClick = () => {
+        navigator.clipboard.writeText(email)
+        .then(() => {
+            alert("Email copied to clipboard");
+        })
+        .catch(err => {
+            console.error("Failed to copy: ", err);
+        });
+    };
+
     return(
         <div className='NameHeader'>
             <div className='Name'>William Bagley</div>
             <div className='Container'>
-                <div className='Description'>Competitive Alpha Geek</div>
+                <div className='Description'>{descriptionText}</div>
                 <img src= '/images/william.png' className='RealImage' alt=''/>
             </div>
             <div className='ImageIcons'>
@@ -17,6 +30,7 @@ function NameHeader() {
                 <a href='https://github.com/wbagley01'>
                     <img src= '/images/github.png'/>
                 </a>
+                <img src= '/images/gmail.png' onClick={handleImageClick}></img>
             </div>
         </div>
     )
