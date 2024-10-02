e8da30b8129aa25378dc1d70c02212a3af3e66f5
wbagleyAsu <wbagley01@gmail.com>
2024-10-01 18:09:12 -0700

    education/project card

--- src/components/education/EducationCard.js
+++ src/components/education/EducationCard.js
@@ -3,9 +3,31 @@ import App from '../../App'
 import './EducationCard.css'
 
 function EducationCard() {
+    const university = "Arizona State University";
+    const school = "Ira A. Fulton Schools of Engineering";
+    const degree = "Computer Science";
+    const degreetype = "Bachelor of Science";
+    const gpa = "GPA: 3.67";
+    const eyears = "2020 - 2024";
+    const location = "Tempe, Arizona";
+
+    const certificate = "Computer Gaming";
+    const cyears = "2022 -2024";
     return(
-        <div>
-            Education Card
+        <div className='EducationCard'>
+            <div className='Container'>
+                {university}
+                <a href='https://www.asu.edu/'>
+                    <img src= '/images/asu.png' style={{maxWidth: '100px', maxHeight: '100px'}}/>
+                </a>
+            </div>
+            <div> {degreetype} {degree} </div>
+            <div> {school} </div>
+            <div> {location} </div>
+            <div> {gpa} </div>
+            <div> {eyears} </div>
+            <div> {certificate} </div>
+            <div> {cyears} </div>
         </div>
     )
 }
--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -2,11 +2,22 @@ import React from 'react';
 import App from '../../App'
 import './ProjectCard.css'
 
+function ProjectSlot({title, description, image, techStack}) {
+    return(
+        <div className='ProjectCard'>
+            <div className='Container'>
+                <header> {title} </header>
+                <img src={image}></img>
+            </div>
+            <p> {description} </p>
+            <p> {techStack} </p>
+        </div>
+    )
+}
+
 function ProjectCard() {
     return(
-        <div>
-            Project Card
-        </div>
+        <ProjectSlot title="Test" description="Test2" image="/images/linkedin.png" techStack="test" />
     )
 }
 
